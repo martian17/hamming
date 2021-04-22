dab2eab920a77980af15810cfdb15646c1be1039
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-04-22 22:14:00 +0900

    it works now

--- main.js
+++ main.js
@@ -2,41 +2,79 @@
 
 var Hamming = function(n){//2 to the n  bits of parity
     var blockSize = 1<<n;
-    var blockDataSize = blockSize - n;
+    var blockDataSize = blockSize - n - 1;
     this.encode = function(bits){
-        var arr = [];
+        var blocks = [];
         var idx = 0;//index of the bit
         
         while(idx < bits.length){
-            var block = [];
-            arr.push(block);
+            var block = [0];
+            blocks.push(block);
             var cnt = 0;
-            var prev = 0;
+            for(var k = 0; k < n; k++){
+                block[1<<k] = 0;//initializing
+            }
             for(var i = 0; i < n; i++){
                 var thisval = 1<<i;
-                for(var j = prev; j < thisval; j++){
+                var nextval = 1<<(i+1);
+                for(var j = thisval+1; j < nextval; j++){
                     block[j] = bits[idx] || 0;
                     idx++;
                     //nowincrementing the parity bits
                     for(var k = 0; k < n; k++){
                         //xor equal
-                        block[1<<k] ^= ((j>>k)&1)&(bits[idx] || 0);
+                        block[1<<k] ^= ((j>>k)&1)&(block[j] || 0);
                     }
                 }
-                prev = thisval+1;
             }
             //block filled
         }
-        return arr;
+        return blocks;
     };
     
-    this.decode = function(bits){
+    this.decode = function(blocks){
+        var data = [];
+        for(var ii = 0; ii < blocks.length; ii++){
+            var block = blocks[ii];
+            var parities = [];
+            
+            for(var i = 0; i < n; i++){
+                var thisval = 1<<i;
+                var nextval = 1<<(i+1);
+                for(var j = thisval+1; j < nextval; j++){
+                    //now incrementing the parity bits
+                    for(var k = 0; k < n; k++){
+                        block[1<<k] ^= ((j>>k)&1)&(block[j] || 0);
+                        //this should be 0 by the end
+                    }
+                }
+            }
+            //tally of parity is the wrong address
+            var address = 0;
+            for(var k = 0; k < n; k++){
+                address += block[1<<k]<<k;
+            }
+            block[address] = [(block[address]^1)];//flip it and mark it
+        }
         
+        for(var i = 0; i < n; i++){
+            var thisval = 1<<i;
+            var nextval = 1<<(i+1);
+            for(var j = thisval+1; j < nextval; j++){
+                data.push(block[j]);
+            }
+        }
+        return data;
     };
 };
 
 var hamming = new Hamming(5);
 
-var data = hamming.encode([0,1,1,0,1,0,1,0]);
-
-console.log(data);
+var data0 = [0,1,1,0,1,0,1,0];
+console.log(data0);
+var coded = hamming.encode(data0);
+coded[0][5] ^= 1;
+//introducing error
+console.log(coded);
+var data1 = hamming.decode(coded);
+console.log(data1);
